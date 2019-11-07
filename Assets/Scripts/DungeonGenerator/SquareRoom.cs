6794b4d9a24e7f92d91a87255d95e0a1bed6fa31
MrVenik <vladp1998@gmail.com>
2019-11-07 13:21:32 +0200

    Fix Square Room CanCreate checking

--- Assets/Scripts/DungeonGenerator/SquareRoom.cs
+++ Assets/Scripts/DungeonGenerator/SquareRoom.cs
@@ -6,5 +6,13 @@ namespace DungeonGenerator
 {
     public class SquareRoom : TemplateRoom
     {
+        public override bool CanCreate(int x, int y)
+        {
+            if (DungeonManager.Dungeon.GetRoom(x, y + 1) is SquareRoom) return false;
+            if (DungeonManager.Dungeon.GetRoom(x, y - 1) is SquareRoom) return false;
+            if (DungeonManager.Dungeon.GetRoom(x - 1, y) is SquareRoom) return false;
+            if (DungeonManager.Dungeon.GetRoom(x + 1, y) is SquareRoom) return false;
+            return base.CanCreate(x, y);
+        }
     }
 }
