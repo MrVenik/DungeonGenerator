291cd4d2dbf1357061677af0cb081f2abfda60a5
MrVenik <vladp1998@gmail.com>
2019-12-05 15:28:21 +0200

    Included Radius CanCreate method into Room CanCreate
    Needed for SquareRoom

--- Assets/Scripts/DungeonGenerator/RoomChecker.cs
+++ Assets/Scripts/DungeonGenerator/RoomChecker.cs
@@ -9,10 +9,24 @@ namespace DungeonGenerator
 {
     public abstract class RoomChecker : ScriptableObject
     {
+        [SerializeField] private int _radius;
         public virtual bool CanCreate(int x, int y, RoomBehaviour room)
         {
             if (DungeonManager.Dungeon.GetRoom(x, y) != null) return false;
             if (room.Size > DungeonManager.Dungeon.MaximumRoomSize) return false;
+
+            if (_radius > 0)
+            {
+                for (int ix = x - _radius; ix <= x + _radius; ix++)
+                {
+                    for (int iy = y - _radius; iy <= y + _radius; iy++)
+                    {
+                        RoomBehaviour nextRoom = DungeonManager.Dungeon.GetRoom(ix, iy);
+                        if (nextRoom != null && nextRoom.GetType() == room.GetType()) return false;
+                    }
+                }
+            }
+
             return true;
         }
     }
