3472101d8235fa2ab334fbec6f05a99066e057ad
MrVenik <vladp1998@gmail.com>
2019-12-05 14:58:25 +0200

    Included Corridor CanCreate method into Procedural CanCreate

--- Assets/Scripts/DungeonGenerator/ProceduralRoomChecker.cs
+++ Assets/Scripts/DungeonGenerator/ProceduralRoomChecker.cs
@@ -10,22 +10,38 @@ namespace DungeonGenerator
     [CreateAssetMenu(fileName = "New RoomChecker", menuName = "Room Checkers/Procedural Room Checker")]
     public class ProceduralRoomChecker : RoomChecker
     {
+        [Range(0, 4)]
+        [SerializeField] private int _minAmountOfOpenConnections;
+        [Range(0, 4)]
+        [SerializeField] private int _maxAmountOfOpenConnections;
+
         public override bool CanCreate(int x, int y, RoomBehaviour room)
         {
             if (base.CanCreate(x, y, room))
             {
                 ProceduralRoomBehaviour proceduralRoom = room as ProceduralRoomBehaviour;
+                int connections = 0;
+
 
                 Connection topConnection = DungeonManager.Dungeon.GetRoomConnection(x, y + 1);
                 if (!proceduralRoom.PossibleConnectionTypes.Contains(topConnection.Bottom) && topConnection.Bottom != ConnectionType.None) return false;
+                if (topConnection.Bottom != ConnectionType.Wall && topConnection.Bottom != ConnectionType.Border) connections++;
+
                 Connection bottomConnection = DungeonManager.Dungeon.GetRoomConnection(x, y - 1);
                 if (!proceduralRoom.PossibleConnectionTypes.Contains(bottomConnection.Top) && bottomConnection.Top != ConnectionType.None) return false;
+                if (bottomConnection.Top != ConnectionType.Wall && bottomConnection.Top != ConnectionType.Border) connections++;
+
                 Connection leftConnection = DungeonManager.Dungeon.GetRoomConnection(x - 1, y);
                 if (!proceduralRoom.PossibleConnectionTypes.Contains(leftConnection.Right) && leftConnection.Right != ConnectionType.None) return false;
+                if (leftConnection.Right != ConnectionType.Wall && leftConnection.Right != ConnectionType.Border) connections++;
+
                 Connection rightConnection = DungeonManager.Dungeon.GetRoomConnection(x + 1, y);
                 if (!proceduralRoom.PossibleConnectionTypes.Contains(rightConnection.Left) && rightConnection.Left != ConnectionType.None) return false;
+                if (rightConnection.Left != ConnectionType.Wall && rightConnection.Left != ConnectionType.Border) connections++;
 
-                return true;
+                bool canCreate = (connections >= _minAmountOfOpenConnections && connections <= _maxAmountOfOpenConnections);
+
+                return canCreate;
             }
             else return false;
         }
