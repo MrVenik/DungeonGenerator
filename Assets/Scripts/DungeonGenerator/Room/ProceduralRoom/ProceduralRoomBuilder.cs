355e66cdeac6bd8712c77469e7ace91e44049546
MrVenik <vladp1998@gmail.com>
2019-12-13 04:18:14 +0200

    New ProceduralRoomBuilder

--- Assets/Scripts/DungeonGenerator/Room/ProceduralRoom/ProceduralRoomBuilder.cs
+++ Assets/Scripts/DungeonGenerator/Room/ProceduralRoom/ProceduralRoomBuilder.cs
@@ -7,71 +7,206 @@ using UnityEngine;
 
 namespace DungeonGenerator
 {
+    public enum RoomCellData
+    {
+        None, 
+        Wall,
+        Floor
+    }
+
     [CreateAssetMenu(fileName = "New RoomBuilder", menuName = "Rooms/Room Builders/Procedural Room Builder")]
     public class ProceduralRoomBuilder : RoomBuilder
     {
-        [SerializeField] private List<GameObject> _columnVariants;
-        [SerializeField] private List<GameObject> _wallConnectionVariants;
-        [SerializeField] private List<GameObject> _smallConnectionVariants;
-        [SerializeField] private List<GameObject> _mediumConnectionVariants;
-        [SerializeField] private List<GameObject> _bigConnectionVariants;
-        [SerializeField] private List<GameObject> _secretConnectionVariants;
+        [SerializeField] private List<GameObject> _wallVariants;
 
-        public override void Build(RoomData roomData, Transform transform)
+        private RoomCellData[,] _roomCells;
+
+        public void PlaceWalls(RoomData roomData)
         {
             int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
-            int roomSize = (int)roomData.Size;
+            _roomCells = new RoomCellData[maximumSize, maximumSize];
 
-            int diff = (maximumSize - roomSize) / 2;
-
-            Instantiate(GetVariantFrom(_columnVariants), new Vector3(transform.position.x + diff, transform.position.y + diff), transform.rotation, transform);
-            Instantiate(GetVariantFrom(_columnVariants), new Vector3(transform.position.x + diff, transform.position.y + maximumSize - 1 - diff), transform.rotation, transform);
-            Instantiate(GetVariantFrom(_columnVariants), new Vector3(transform.position.x + maximumSize - 1 - diff, transform.position.y + diff), transform.rotation, transform);
-            Instantiate(GetVariantFrom(_columnVariants), new Vector3(transform.position.x + maximumSize - 1 - diff, transform.position.y + maximumSize - 1 - diff), transform.rotation, transform);
-
-            GameObject element;
-            // Top connection
-            element = GetConnectionGameObject(roomData.Connection.Top);
-            if (element != null)
-                Instantiate(element, new Vector3(transform.position.x + 1 + diff, transform.position.y - 1 + maximumSize - diff), Quaternion.Euler(0, 0, 0), transform);
-            // Bottom connection
-            element = GetConnectionGameObject(roomData.Connection.Bottom);
-            if (element != null)
-                Instantiate(element, new Vector3(transform.position.x - 1 + maximumSize - diff, transform.position.y + 1 + diff), Quaternion.Euler(0, 0, 180), transform);
-            // Left connection
-            element = GetConnectionGameObject(roomData.Connection.Left);
-            if (element != null)
-                Instantiate(element, new Vector3(transform.position.x + 1 + diff, transform.position.y + 1 + diff), Quaternion.Euler(0, 0, 90), transform);
-            // Ritght connection
-            element = GetConnectionGameObject(roomData.Connection.Right);
-            if (element != null)
-                Instantiate(element, new Vector3(transform.position.x + maximumSize - 1 - diff, transform.position.y + maximumSize - 1 - diff), Quaternion.Euler(0, 0, -90), transform);
-
-            if (FloorBuilder != null) FloorBuilder.Build(roomData, transform);
+            BuildSide(roomData, Side.Top);
+            BuildSide(roomData, Side.Bottom);
+            BuildSide(roomData, Side.Left);
+            BuildSide(roomData, Side.Right);
         }
 
-        private GameObject GetConnectionGameObject(ConnectionType type)
+        private void BuildSide(RoomData roomData, Side side)
         {
-            switch (type)
+            switch (roomData.Connection.GetConnectionTypeBySide(side))
             {
                 case ConnectionType.None:
-                    return null;
+                    break;
                 case ConnectionType.Border:
-                    return GetVariantFrom(_wallConnectionVariants);
+                    break;
                 case ConnectionType.Wall:
-                    return GetVariantFrom(_wallConnectionVariants);
-                case ConnectionType.Medium:
-                    return GetVariantFrom(_mediumConnectionVariants);
+                    BuildWall(roomData, side);
+                    break;
                 case ConnectionType.Small:
-                    return GetVariantFrom(_smallConnectionVariants);
+                    BuildOpen(roomData, side, 2);
+                    break;
+                case ConnectionType.Medium:
+                    BuildOpen(roomData, side, 3);
+                    break;
                 case ConnectionType.Big:
-                    return GetVariantFrom(_bigConnectionVariants);
+                    BuildOpen(roomData, side, 4);
+                    break;
                 case ConnectionType.SecretRoomDoor:
-                    return GetVariantFrom(_secretConnectionVariants);
+                    break;
                 default:
                     break;
             }
-            throw new Exception("Invalid connection type " + type);
+        }
+
+        private void BuildWall(RoomData roomData, Side side)
+        {
+            int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
+            int roomSize = (int)roomData.Size;
+            int offset = (maximumSize - roomSize) / 2;
+
+            switch (side)
+            {
+                case Side.Top:
+                    for (int x = offset; x < maximumSize - offset; x++)
+                    {
+                        if (_roomCells[x, maximumSize - offset - 1] == RoomCellData.None)
+                        {
+                            _roomCells[x, maximumSize - offset - 1] = RoomCellData.Wall;
+                        }
+                    }
+
+
+                    break;
+                case Side.Bottom:
+                    for (int x = offset; x < maximumSize - offset; x++)
+                    {
+                        if (_roomCells[x, offset] == RoomCellData.None)
+                        {
+                            _roomCells[x, offset] = RoomCellData.Wall;
+                        }
+                    }
+
+                    break;
+                case Side.Left:
+                    for (int y = offset; y < maximumSize - offset; y++)
+                    {
+                        if (_roomCells[offset, y] == RoomCellData.None)
+                        {
+                            _roomCells[offset, y] = RoomCellData.Wall;
+                        }
+                    }
+
+                    break;
+                case Side.Right:
+                    for (int y = offset; y < maximumSize - offset; y++)
+                    {
+                        if (_roomCells[maximumSize - offset - 1, y] == RoomCellData.None)
+                        {
+                            _roomCells[maximumSize - offset - 1, y] = RoomCellData.Wall;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void BuildOpen(RoomData roomData, Side side, int connectionOffset)
+        {
+            int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
+            int roomSize = (int)roomData.Size;
+            int offset = (maximumSize - roomSize) / 2;
+            int centerPoint = (maximumSize - 1) / 2;
+
+
+            switch (side)
+            {
+                case Side.Top:
+                    for (int y = maximumSize - offset - 1; y < maximumSize; y++)
+                    {
+                        for (int x = offset; x < maximumSize - offset; x++)
+                        {
+                            if ((y == maximumSize - offset - 1) && _roomCells[x, maximumSize - offset - 1] == RoomCellData.None && !(x > centerPoint - connectionOffset && x < centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                            else if (_roomCells[x, y] == RoomCellData.None && (x == centerPoint - connectionOffset || x == centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                        }
+                    }
+                    break;
+                case Side.Bottom:
+                    for (int y = 0; y < offset + 1; y++)
+                    {
+                        for (int x = offset; x < maximumSize - offset; x++)
+                        {
+                            if (y == offset && _roomCells[x, y] == RoomCellData.None && !(x > centerPoint - connectionOffset && x < centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                            else if(_roomCells[x, y] == RoomCellData.None && (x == centerPoint - connectionOffset || x == centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                        }
+                    }
+                    break;
+                case Side.Left:
+                    for (int x = 0; x < offset + 1; x++)
+                    {
+                        for (int y = offset; y < maximumSize - offset; y++)
+                        {
+                            if (x == offset && _roomCells[x, y] == RoomCellData.None && !(y > centerPoint - connectionOffset && y < centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                            else if(_roomCells[x, y] == RoomCellData.None && (y == centerPoint - connectionOffset || y == centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                        }
+                    }
+                    break;
+                case Side.Right:
+                    for (int x = maximumSize - offset - 1; x < maximumSize; x++)
+                    {
+                        for (int y = offset; y < maximumSize - offset; y++)
+                        {
+                            if ((x == maximumSize - offset - 1) && _roomCells[maximumSize - offset - 1, y] == RoomCellData.None && !(y > centerPoint - connectionOffset && y < centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                            else if (_roomCells[x, y] == RoomCellData.None && (y == centerPoint - connectionOffset || y == centerPoint + connectionOffset))
+                            {
+                                _roomCells[x, y] = RoomCellData.Wall;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override void Build(RoomData roomData, Transform transform)
+        {
+            PlaceWalls(roomData);
+
+            int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
+
+            for (int x = 0; x < maximumSize; x++)
+            {
+                for (int y = 0; y < maximumSize; y++)
+                {
+                    if (_roomCells[x, y] == RoomCellData.Wall)
+                    {
+                        Instantiate(GetVariantFrom(_wallVariants), new Vector3(transform.position.x + x, transform.position.y + y), transform.rotation, transform);
+                    }
+                }
+            }
         }
 
         private GameObject GetVariantFrom(List<GameObject> variants)
