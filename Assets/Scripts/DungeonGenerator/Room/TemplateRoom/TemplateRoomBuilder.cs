19675a38ecef2f41c17290e3ba7c33b43227ce9c
MrVenik <vladp1998@gmail.com>
2019-12-20 03:26:22 +0200

    New TemplateRoomBuilder based on string template
    It is only for testing

--- Assets/Scripts/DungeonGenerator/Room/TemplateRoom/TemplateRoomBuilder.cs
+++ Assets/Scripts/DungeonGenerator/Room/TemplateRoom/TemplateRoomBuilder.cs
@@ -11,33 +11,258 @@ namespace DungeonGenerator
     [CreateAssetMenu(fileName = "New RoomBuilder", menuName = "Rooms/Room Builders/Template Room Builder")]
     public class TemplateRoomBuilder : RoomBuilder
     {
-        [SerializeField] private GameObject _templatePrefab;
+        [SerializeField] private List<WallData> _wallVariants;
+        [SerializeField] private List<Tile> _floorVariants;
+
+        private string[] _template = {  "###___###",
+                                        "#_______#",
+                                        "#_______#",
+                                        "___###___",
+                                        "___# #___",
+                                        "___###___",
+                                        "#_______#",
+                                        "#_______#",
+                                        "###___###" };
+
+
+
+        private RoomCellData[,] _roomCells;
+
+        public void PlaceCells(RoomData roomData)
+        {
+            int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
+            _roomCells = new RoomCellData[maximumSize, maximumSize];
+
+            for (int x = 0; x < maximumSize; x++)
+            {
+                for (int y = 0; y < maximumSize; y++)
+                {
+                    if (_template[x][y] == ' ')
+                    {
+                        _roomCells[x, y] = RoomCellData.None;
+                    }
+                    else if (_template[x][y] == '#')
+                    {
+                        _roomCells[x, y] = RoomCellData.Wall;
+                    }
+                    else if (_template[x][y] == '_')
+                    {
+                        _roomCells[x, y] = RoomCellData.Floor;
+                    }
+                }
+            }
+
+        }
+
         public override void Build(RoomData roomData, Vector3Int position, TilemapData tilemapData)
         {
-            /*
             int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
-            int roomSize = (int)roomData.Size;
 
+            PlaceCells(roomData);
 
-            int diff = (maximumSize - roomSize) / 2;
-
-            switch (roomData.Entrance)
+            for (int x = 0; x < maximumSize; x++)
             {
-                case Side.Top:
-                    Instantiate(_templatePrefab, new Vector3(transform.position.x + maximumSize - diff, transform.position.y + maximumSize - diff), Quaternion.Euler(0, 0, 180), transform);
-                    break;
-                case Side.Bottom:
-                    Instantiate(_templatePrefab, new Vector3(transform.position.x + diff, transform.position.y + diff), Quaternion.Euler(0, 0, 0), transform);
-                    break;
-                case Side.Left:
-                    Instantiate(_templatePrefab, new Vector3(transform.position.x + diff, transform.position.y + maximumSize - diff), Quaternion.Euler(0, 0, 270), transform);
-                    break;
-                case Side.Right:
-                    Instantiate(_templatePrefab, new Vector3(transform.position.x + maximumSize - diff, transform.position.y + diff), Quaternion.Euler(0, 0, 90), transform);
-                    break;
-                default:
-                    break;
-            }*/
+                for (int y = 0; y < maximumSize; y++)
+                {
+                    Vector3Int tilePosition = new Vector3Int(position.x + x, position.y + y, 0);
+
+                    if (_roomCells[x, y] == RoomCellData.Wall)
+                    {
+                        PlaceWall(x, y, position, tilemapData.WallLayer);
+                    }
+                    else if (_roomCells[x, y] == RoomCellData.Floor)
+                    {
+                        if (!tilemapData.FloorLayer.HasTile(tilePosition))
+                        {
+                            tilemapData.FloorLayer.SetTile(tilePosition, _floorVariants.GetRandomElement());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PlaceWall(int x, int y, Vector3Int roomPosition, Tilemap wallLayer)
+        {
+            int maximumSize = (int)DungeonManager.Dungeon.MaximumRoomSize;
+
+            // placing top wall
+            if (y - 1 >= 0 && _roomCells[x, y - 1] == RoomCellData.Floor)
+            {
+                if (y + 1 >= maximumSize || _roomCells[x, y + 1] != RoomCellData.Floor)
+                {
+                    // Placing brick
+                    Vector3Int brickTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y, 0);
+                    if (!wallLayer.HasTile(brickTilePosition))
+                    {
+                        wallLayer.SetTile(brickTilePosition, _wallVariants.GetRandomElement().Brick);
+                    }
+                    // Placing top left inner
+                    if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Floor)
+                    {
+                        Vector3Int topTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                        if (!wallLayer.HasTile(topTilePosition))
+                        {
+                            wallLayer.SetTile(topTilePosition, _wallVariants.GetRandomElement().TopInnerLeft);
+                        }
+                    }
+                    // Placing top right inner
+                    else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Floor)
+                    {
+                        Vector3Int topTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                        if (!wallLayer.HasTile(topTilePosition))
+                        {
+                            wallLayer.SetTile(topTilePosition, _wallVariants.GetRandomElement().TopInnerRight);
+                        }
+                    }
+                    // Placing top
+                    else
+                    {
+                        Vector3Int topTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                        if (!wallLayer.HasTile(topTilePosition))
+                        {
+                            wallLayer.SetTile(topTilePosition, _wallVariants.GetRandomElement().Top);
+                        }
+                    }
+                }
+            }
+
+            // placing bottom wall
+            if (y + 1 < maximumSize && _roomCells[x, y + 1] == RoomCellData.Floor)
+            {
+                // Placing bottom left inner
+                if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Floor)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().BottomInnerLeft);
+                    }
+                }
+                // Placing bottom right inner
+                else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Floor)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().BottomInnerRight);
+                    }
+                }
+                // Placing bottom
+                else
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().Bottom);
+                    }
+                }
+            }
+
+            // placing side walls
+            if (y - 1 >= 0 && y + 1 < maximumSize)
+            {
+                if (_roomCells[x, y - 1] == RoomCellData.Wall && _roomCells[x, y + 1] == RoomCellData.Wall)
+                {
+                    // Placing left
+                    if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Floor)
+                    {
+                        Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                        if (!wallLayer.HasTile(bottomTilePosition))
+                        {
+                            wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopLeft);
+                        }
+                    }
+                    // Placing right
+                    else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Floor)
+                    {
+                        Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                        if (!wallLayer.HasTile(bottomTilePosition))
+                        {
+                            wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopRight);
+                        }
+                    }
+                }
+            }
+            // placing top corners
+            if (y + 1 >= maximumSize || _roomCells[x, y + 1] == RoomCellData.None)
+            {
+                // Placing left outer
+                if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Wall)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopOuterLeft);
+                    }
+                }
+                // Placing left 
+                else if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Floor)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopLeft);
+                    }
+                }
+                // Placing right outer
+                else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Wall)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopOuterRight);
+                    }
+                }
+                // Placing right 
+                else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Floor)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopRight);
+                    }
+                }
+            }
+            // placing bottom corners
+            if (y - 1 < 0 || _roomCells[x, y - 1] == RoomCellData.None)
+            {
+                // Placing left outer
+                if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Wall)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().BottomOuterLeft);
+                    }
+                }
+                // Placing left 
+                else if (x + 1 < maximumSize && _roomCells[x + 1, y] == RoomCellData.Floor)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopLeft);
+                    }
+                }
+                // Placing right outer
+                else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Wall)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().BottomOuterRight);
+                    }
+                }
+                // Placing right 
+                else if (x - 1 >= 0 && _roomCells[x - 1, y] == RoomCellData.Floor)
+                {
+                    Vector3Int bottomTilePosition = new Vector3Int(roomPosition.x + x, roomPosition.y + y + 1, 0);
+                    if (!wallLayer.HasTile(bottomTilePosition))
+                    {
+                        wallLayer.SetTile(bottomTilePosition, _wallVariants.GetRandomElement().TopRight);
+                    }
+                }
+            }
         }
     }
 }
